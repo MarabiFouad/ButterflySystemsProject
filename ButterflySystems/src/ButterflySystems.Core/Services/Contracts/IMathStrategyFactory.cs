3dee657c0738eaa697a2206c00dbc3ea457fc8b5
Fouad Marabi <fouad@homedish.co.nz>
2022-06-24 11:12:05 +0400

    comment on some interfaces

--- ButterflySystems/src/ButterflySystems.Core/Services/Contracts/IMathStrategyFactory.cs
+++ ButterflySystems/src/ButterflySystems.Core/Services/Contracts/IMathStrategyFactory.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace ButterflySystems.Core.Services.Contracts
 {
+    /// <summary>
+    /// IMathStrategyFactory is used to initialize all the implementation of IMathOperator
+    /// </summary>
     public interface IMathStrategyFactory
     {
+        /// <summary>
+        /// Asp Core IoC container will use this method to inject IMathOperator[] type.
+        /// </summary>
+        /// <returns></returns>
         IMathOperator[] Create();
     }
 }
